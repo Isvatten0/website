23c3e8ffc55cb0ea7d1493816d159d2d3f23cfbd
Luke Roberts <104106765+Isvatten0@users.noreply.github.com>
2024-06-11 16:37:28 -0400

    change to local

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -18,39 +18,27 @@ export const help = async (args: string[]): Promise<string> => {
 \n${c}\n
 [tab]: trigger completion.
 [ctrl+l]/clear: clear terminal.\n
-Type 'sumfetch' to display summary.
+Type 'summary' to display summary.
 `;
-};
-
-// Redirection
-export const repo = async (args: string[]): Promise<string> => {
-  window.open(`${config.repo}`);
-  return 'Opening Github repository...';
 };
 
 // About
 export const about = async (args: string[]): Promise<string> => {
   return `Hi, I am ${config.name}. 
-Welcome to my website!
+Welcome to my website! 
+An "unbiased" source tells me you are going to 
+find exactly what you were looking for. ;)
 More about me:
 'sumfetch' - short summary.
-'resume' - my latest resume.
-'readme' - my github readme.`;
+'resume' - my latest resume.`;
 };
 
 export const resume = async (args: string[]): Promise<string> => {
-  window.open(`${config.resume_url}`);
+  window.open('EmployedResume.pdf');
   return 'Opening resume...';
 };
 
-// Donate
-export const donate = async (args: string[]): Promise<string> => {
-  return `thank you for your interest. 
-here are the ways you can support my work:
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
-`;
-};
+
 
 // Contact
 export const email = async (args: string[]): Promise<string> => {
@@ -70,20 +58,16 @@ export const linkedin = async (args: string[]): Promise<string> => {
   return 'Opening linkedin...';
 };
 
+export const danceparty = async (args: string[]): Promise<string> => {
+  window.open(`https://www.youtube.com/watch?v=1d9Gebi9Kus`);
+
+  return 'Opening github...';
+};
+
 // Search
 export const google = async (args: string[]): Promise<string> => {
   window.open(`https://google.com/search?q=${args.join(' ')}`);
   return `Searching google for ${args.join(' ')}...`;
-};
-
-export const duckduckgo = async (args: string[]): Promise<string> => {
-  window.open(`https://duckduckgo.com/?q=${args.join(' ')}`);
-  return `Searching duckduckgo for ${args.join(' ')}...`;
-};
-
-export const bing = async (args: string[]): Promise<string> => {
-  window.open(`https://bing.com/search?q=${args.join(' ')}`);
-  return `Wow, really? You are using bing for ${args.join(' ')}?`;
 };
 
 export const reddit = async (args: string[]): Promise<string> => {
@@ -101,16 +85,29 @@ export const whoami = async (args: string[]): Promise<string> => {
 };
 
 export const ls = async (args: string[]): Promise<string> => {
-  return `a
-bunch
-of
-fake
-directories`;
+  return `LukeFolder
+SeemsFolder
+LikeFolder
+AnFolder
+AmazingFolder
+CandidateFolder`;
+};
+
+export const summary = async (args: string[]): Promise<string> => {
+  return `I graduated from the College of Engineering at the University of Alabama with a bachelors degree in Computer Science.
+  I have experience in data collection, transformation, and organization for analysis,
+  with excellent understanding and proficiency in platforms like Linux and languages such as SQL, Python, and Qlik.
+  I am always trying to learn more each and everyday, completing certificates for funsies and
+  hopping aboard the Neet/Leetcode train. In my free time, I am developing games and building an interactive website (as you can see lol ).
+  So far I've made a zombie survival game in Godot as well as a Monster battle checkers game in Pygame.
+  
+  oh, and also I'm proficient in Excel... ;)
+  ...But seriously I use it daily.`;
 };
 
 export const cd = async (args: string[]): Promise<string> => {
-  return `unfortunately, i cannot afford more directories.
-if you want to help, you can type 'donate'.`;
+  return `unfortunately, I cannot play any cds as this is a website.
+but if you want to change directories, go to a terminal on your computer and type "cd"'.`;
 };
 
 export const date = async (args: string[]): Promise<string> => {
@@ -141,17 +138,32 @@ export const sudo = async (args?: string[]): Promise<string> => {
 // Banner
 export const banner = (args?: string[]): string => {
   return `
-█████        ███                       ███████████                                   
-░░███        ░░░                       ░█░░░███░░░█                                   
- ░███        ████  █████ █████  ██████ ░   ░███  ░   ██████  ████████  █████████████  
- ░███       ░░███ ░░███ ░░███  ███░░███    ░███     ███░░███░░███░░███░░███░░███░░███ 
- ░███        ░███  ░███  ░███ ░███████     ░███    ░███████  ░███ ░░░  ░███ ░███ ░███ 
- ░███      █ ░███  ░░███ ███  ░███░░░      ░███    ░███░░░   ░███      ░███ ░███ ░███ 
- ███████████ █████  ░░█████   ░░██████     █████   ░░██████  █████     █████░███ █████
-░░░░░░░░░░░ ░░░░░    ░░░░░     ░░░░░░     ░░░░░     ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ 
 
+  
+  █████                  █████                                                           
+  ░░███                  ░░███                                                            
+   ░███        █████ ████ ░███ █████  ██████                                              
+   ░███       ░░███ ░███  ░███░░███  ███░░███                                             
+   ░███        ░███ ░███  ░██████░  ░███████                                              
+   ░███      █ ░███ ░███  ░███░░███ ░███░░░                                               
+   ███████████ ░░████████ ████ █████░░██████                                              
+  ░░░░░░░░░░░   ░░░░░░░░ ░░░░ ░░░░░  ░░░░░░                                               
+                                                                                          
+                                                                                          
+                                                                                          
+   ███████████            █████                         █████                        ███  
+  ░░███░░░░░███          ░░███                         ░░███                        ░░███ 
+   ░███    ░███   ██████  ░███████   ██████  ████████  ███████    █████           ██ ░░███
+   ░██████████   ███░░███ ░███░░███ ███░░███░░███░░███░░░███░    ███░░           ░░   ░███
+   ░███░░░░░███ ░███ ░███ ░███ ░███░███████  ░███ ░░░   ░███    ░░█████               ░███
+   ░███    ░███ ░███ ░███ ░███ ░███░███░░░   ░███       ░███ ███ ░░░░███              ███ 
+   █████   █████░░██████  ████████ ░░██████  █████      ░░█████  ██████           ██ ██░  
+  ░░░░░   ░░░░░  ░░░░░░  ░░░░░░░░   ░░░░░░  ░░░░░        ░░░░░  ░░░░░░           ░░ ░░░   
+                                                                                          
+                                                                                          
+                                                                                          
 Type 'help' to see the list of available commands.
-Type 'sumfetch' to display summary.
+Type 'summary' to display summary.
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
 `;
 };
